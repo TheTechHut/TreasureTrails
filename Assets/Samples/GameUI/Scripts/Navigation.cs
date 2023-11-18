f8591e68c124a02cf59cf70694d13f6dd12b3483
STARKthegreat <emmanuelkipropkimutai@gmail.com>
2023-11-18 03:50:02 +0300

    added firebase auth pack

--- Assets/Samples/GameUI/Scripts/Navigation.cs
+++ Assets/Samples/GameUI/Scripts/Navigation.cs
@@ -1,11 +1,37 @@
+namespace NaviagtionSpace
+{
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Navigation : MonoBehaviour{
 
-    public void OpenGeospatialScene()
-    {
-        SceneManager.LoadScene("Geospatial");
-        Debug.Log("Navigated to home page");
-    }
+public GameObject coming_soon_overlay;
+
+public void OpenGeospatialScene()
+{
+SceneManager.LoadScene("Geospatial");
+Debug.Log("Navigated to gameplay page");
 }
+
+public void OnComingSoonTrigger()
+{
+    float timer = 3.0f;
+    timer -= Time.deltaTime;
+
+    if(timer<0)
+    {
+        coming_soon_overlay.SetActive(false);
+    }
+
+    coming_soon_overlay.SetActive(true);
+}
+}
+}
+
