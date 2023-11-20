ebb61e5cdd9c6a32d646b7002d67920dafbdf8bb
STARKthegreat <emmanuelkipropkimutai@gmail.com>
2023-11-20 15:23:18 +0300

    adds models used

--- Assets/Samples/GameUI/Scripts/OnBoarding.cs
+++ Assets/Samples/GameUI/Scripts/OnBoarding.cs
@@ -3,9 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class OnBoarding : MonoBehaviour{
 
+    public GameObject overlay;
     public void OpenHomePageScene()
     {
         SceneManager.LoadScene("HomeScene");
         Debug.Log("Navigated to home page");
+    }
+
+    public void OpenOverlay()
+    {
+        overlay.SetActive(true);
+    }
+
+     public void CloseOverlay()
+    {
+        overlay.SetActive(false);
     }
 }
